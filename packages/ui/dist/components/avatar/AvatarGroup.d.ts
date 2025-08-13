3a4095b310c56fd9f4c8fa16a47f3b7eb749f7d9
Taha Asif <tahaasifaqwe@gmail.com>
2025-08-13 21:19:51 +0500

    Updated

--- packages/ui/dist/components/avatar/AvatarGroup.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as React } from 'react';
-interface AvatarGroupProps {
-    avatars: string[];
-    size?: "small" | "medium" | "large";
-    maxVisible?: number;
-    className?: string;
-}
-export declare const AvatarGroup: React.FC<AvatarGroupProps>;
-export {};
